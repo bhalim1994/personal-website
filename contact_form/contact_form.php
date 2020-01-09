7a2d1d6d4cb99c8827dce1ab9152a0350cb77daf
bhalim1994 <bhalim1994@gmail.com>
2020-01-09 10:06:19 -0800

    Edited contact_form.php to include domain email

--- contact_form/contact_form.php
+++ contact_form/contact_form.php
@@ -1,8 +1,8 @@
 <?php
 
 // configure
-$from = 'bhalim1994@yourdomain.com';
-$sendTo = 'bhalim1994@gmail.com'; // Replace it with Your email. REQUIRED!
+$from = 'administrator@benedicthalim.com';
+$sendTo = 'bhalim1994@gmail.com';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'email' => 'Email', 'subject' => 'Subject', 'message' => 'Message'); // array variable name => Text to appear in the email. If you added or deleted a field in the contact form, edit this array.
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
